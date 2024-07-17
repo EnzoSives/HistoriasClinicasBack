6c6bd3f6b4e6760c10d6294f0df59ec26175ecdb
EnzoSivesTelpin <158466882+EnzoSivesTelpin@users.noreply.github.com>
2024-07-17 12:20:11 -0300

    Mejoras

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,35 +7,24 @@ import { AuthModule } from './auth/auth.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-
-// @Module({
-//   imports: [
-//     TypeOrmModule.forRoot({
-//       type: 'mysql',
-//       host: 'b0f8arjbbobjpsje6bub-mysql.services.clever-cloud.com',
-//       port: 3306,
-//       username: 'u9ze4aydevqy5xtq',
-//       password: 'h94pLnqUl02TqHdwAcQk',
-//       database: 'b0f8arjbbobjpsje6bub',
-//       entities: [__dirname + '/**/*.entity{.ts,.js}'],
-//       synchronize: true,
-//     }),
-//     UsersModule,
-//     PacienteModule,
-//     ConsultaModule,
-//     AuthModule,
-//   ],
-//   controllers: [AppController],
-//   providers: [AppService],
-// })
 @Module({
   imports: [
+    // TypeOrmModule.forRoot({
+    //   type: 'mysql',
+    //   host: 'localhost',
+    //   port: 3306,
+    //   username: 'root',
+    //   password: 'Cristiano7',
+    //   database: 'b0f8arjbbobjpsje6bub',
+    //   entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    //   synchronize: true,
+    // }),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
+      host: 'bpeyvodevjsywngcdapn-mysql.services.clever-cloud.com',
       port: 3306,
-      username: 'root',
-      password: 'Cristiano7',
+      username: 'u9ze4aydevqy5xtq',
+      password: 'h94pLnqUl02TqHdwAcQk',
       database: 'b0f8arjbbobjpsje6bub',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
@@ -48,4 +37,4 @@ import { AppService } from './app.service';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
