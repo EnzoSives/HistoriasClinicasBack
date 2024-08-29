216c23fc0e4c091229363fe75b38d4ebfdf1d559
EnzoSives <73887784+EnzoSives@users.noreply.github.com>
2024-08-29 10:37:19 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   // Configurar CORS
   app.enableCors({
     // origin: 'https://secretariamadariaga-799ec.web.app',
-    origin: '*',
+    origin: 'http://149.50.137.224:5173/',
      // Reemplaza con el origen de tu frontend
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
