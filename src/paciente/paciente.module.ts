52fe6af87db43372d580102cbbf61721a2962a48
EnzoSivesTelpin <158466882+EnzoSivesTelpin@users.noreply.github.com>
2024-09-01 21:15:12 -0300

    Cambios en ruta

--- src/paciente/paciente.module.ts
+++ src/paciente/paciente.module.ts
@@ -13,7 +13,7 @@ import path, { extname } from 'path';
     MulterModule.register({
       storage: diskStorage({
         destination: (req, file, cb) => {
-          const uploadPath = 'E:/Documents/Proyectos/Sistema-HClinicas/historias-clinicas/public/uploads';
+          const uploadPath = '~/root/App/HistoriasClinicasFront/public/uploads';
           cb(null, uploadPath);
         },
         filename: (req, file, cb) => {
