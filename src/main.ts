99dfd2011904ee97ea6d78ccd85beb27185409bb
EnzoSivesTelpin <158466882+EnzoSivesTelpin@users.noreply.github.com>
2024-09-01 21:19:23 -0300

    Cambios en main

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
   app.enableCors({
     // origin: 'https://secretariamadariaga-799ec.web.app',
     origin: 'http://149.50.137.224:5173/',
+    // origin: 'http://localhost:5173/',
      // Reemplaza con el origen de tu frontend
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
