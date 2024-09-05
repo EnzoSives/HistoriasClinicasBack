5d8667c9ebd8ea0c819a17364256099761174c10
EnzoSives <73887784+EnzoSives@users.noreply.github.com>
2024-09-05 14:06:33 -0300

    Update paciente.module.ts

--- src/paciente/paciente.module.ts
+++ src/paciente/paciente.module.ts
@@ -6,7 +6,6 @@ import { PacienteService } from './paciente.service';
 import { PacienteController } from './paciente.controller';
 import { diskStorage } from 'multer';
 import path, { extname } from 'path';
-import fs from 'fs';
 
 @Module({
   imports: [
@@ -14,15 +13,7 @@ import fs from 'fs';
     MulterModule.register({
       storage: diskStorage({
         destination: (req, file, cb) => {
-          // Construir la ruta al directorio de uploads en el frontend
-          const uploadPath = path.join(__dirname, '../../../HistoriasClinicasFront/public/uploads');
-
-          // Crear el directorio si no existe
-          if (!fs.existsSync(uploadPath)) {
-            fs.mkdirSync(uploadPath, { recursive: true });
-          }
-
-          console.log(`Saving file to: ${uploadPath}`); // Para verificar la ruta
+          const uploadPath = '/root/App/HistoriasClinicasFront/public/uploads';
           cb(null, uploadPath);
         },
         filename: (req, file, cb) => {
