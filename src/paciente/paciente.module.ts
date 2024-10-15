9811501563988410fa58425c7b6e15d14f5c3b37
EnzoSives <73887784+EnzoSives@users.noreply.github.com>
2024-10-15 20:07:43 -0300

    Update paciente.module.ts

--- src/paciente/paciente.module.ts
+++ src/paciente/paciente.module.ts
@@ -13,7 +13,7 @@ import path, { extname } from 'path';
     MulterModule.register({
       storage: diskStorage({
         destination: (req, file, cb) => {
-          const uploadPath = ' /home/historiasclinicas-mada/htdocs/historias-clinicas-mada.com/HistoriasClinicasBack';
+          const uploadPath = '/home/historiasclinicas-mada/htdocs/historias-clinicas-mada.com/HistoriasClinicasBack';
           cb(null, uploadPath);
         },
         filename: (req, file, cb) => {
