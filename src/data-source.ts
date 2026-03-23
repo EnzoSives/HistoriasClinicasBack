0db3a16d3732cf259f10698b2a22786838613e26
Enzo Sives <enzo.sives@telpin.com.ar>
2026-03-23 13:25:12 -0300

    feat: actualizar configuración de conexión a la base de datos usando variables de entorno

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,13 +1,17 @@
 import { DataSource } from 'typeorm';
+import 'dotenv/config';
+
+const dbPort = Number(process.env.DB_PORT ?? '3306');
+const dbSynchronize = (process.env.DB_SYNCHRONIZE ?? 'false') === 'true';
 
 export const AppDataSource = new DataSource({
   type: 'mysql',
-  host: '66.97.45.96',
-  port: 3306,
-  username: 'base_enzo',
-  password: '%#zn7ajqx0qrljLr',
-  database: 'historias_clinicas',
+  host: process.env.DB_HOST ?? 'localhost',
+  port: dbPort,
+  username: process.env.DB_USERNAME ?? 'root',
+  password: process.env.DB_PASSWORD ?? '',
+  database: process.env.DB_DATABASE ?? '',
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
   migrations: [__dirname + '/migrations/*.{ts,js}'],
-  synchronize: false,
+  synchronize: dbSynchronize,
 });
